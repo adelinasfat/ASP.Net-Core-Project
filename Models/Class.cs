3199ddd15f0b4f9cd9718fbb5a96827aeb91d80c
Adelina Sfat <sfatadelina99@gmail.com>
2021-01-09 12:30:38 +0200

    modified regex for class name

--- Models/Class.cs
+++ Models/Class.cs
@@ -12,7 +12,7 @@ namespace Proiect.Models
         public int ID { get; set; }
 
         [Display(Name = "Class Name")]
-        [RegularExpression(@"^[A-Z]\s\W\s[A-Z][a-z]+$", ErrorMessage = "Class name must be of type: 'Letter - Description'"), Required, StringLength(50, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Z]\s\W\s[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Class name must be of type: 'Letter - Description'"), Required, StringLength(50, MinimumLength = 3)]
         public string ClassName { get; set; }
         public ICollection<Classroom> Classrooms { get; set; }
     }
